d5cc4c73411b065208ec92a87d147e78d2113d9c
M.S <mixa.ru.ru@gmail.com>
2022-07-17 16:40:02 +0300

    Dockerfile

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # pfd-huggingface-starter
-Poetry, FastAPI, Docker, Huggingface starter template
+Poetry, FastAPI, Docker, Huggingface transformers starter template
 
 ```bash
+source $(poetry env info --path)/bin/activate
 $HOME/.poetry/bin/poetry shell
 (inside-shell) ~/pfd-huggingface-starter/$ uvicorn src.main:app --reload
+```
+
+```bash
+ poetry add torch --platform linux --python "^3.8"
 ```
 
 Swagger http://127.0.0.1:8000/docs
--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 app = FastAPI()
 
 
 @app.post('/summarize')
 async def get_sum():
+    return {"Hello": "World"}
+
+
+@app.get('/')
+async def load():
+    tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
+    model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
     return {"Hello": "World"}
